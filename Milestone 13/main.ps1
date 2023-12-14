83f248510724f64f688b2f42b2709ecda92596ff
dthomsen116 <71229951+dthomsen116@users.noreply.github.com>
2023-12-14 17:39:28 -0500

    Update main.ps1

--- Milestone 13/main.ps1
+++ Milestone 13/main.ps1
@@ -1,3 +1,5 @@
+# Author: David Thomsen
+# function to grab all VMs on the local Hyper-V server
 function allVmInfo {
     # Get all VMs on the local Hyper-V server
     Get-VM | ForEach-Object {
@@ -11,4 +13,24 @@ function allVmInfo {
     }
 }
 
-allVmInfo
+# function to gather specific VM information with a search for VM name
+
+function specificVMInfo {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$VMName
+    )
+
+    # Get specific VM information with a search for VM name
+    Get-VM -Name $VMName | ForEach-Object {
+        $_ | Select-Object -Property Name, State, 
+        @{Name='IPAddresses'; Expression={($_.NetworkAdapters).IPAddresses}},
+        HardDrives,
+        @{Name='MemoryAssignedMB'; Expression={$_.MemoryAssigned / 1MB}},
+        Uptime, 
+        Status
+        Write-Host "----------------------"
+    }
+}
+
+specificVMInfo -VMName "TempleOS"
