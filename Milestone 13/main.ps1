640c46eaffccfdfd6280432998d55726b2dfa1b9
dthomsen116 <71229951+dthomsen116@users.noreply.github.com>
2023-12-14 17:34:42 -0500

    Update main.ps1

--- Milestone 13/main.ps1
+++ Milestone 13/main.ps1
@@ -1,16 +1,14 @@
-
 function allVmInfo {
     # Get all VMs on the local Hyper-V server
-    Get-VM | Select-Object -Property Name, State, 
-    
-    @{Name='IPAddresses'; Expression={($_.NetworkAdapters).IPAddresses}},
-    HardDrives,
-    
-    @{Name='MemoryAssignedMB'; Expression={$_.MemoryAssigned / 1MB}},
-    Uptime, 
-    Status
-    
-    write-host "---------------------------------"
+    Get-VM | ForEach-Object {
+        $_ | Select-Object -Property Name, State, 
+        @{Name='IPAddresses'; Expression={($_.NetworkAdapters).IPAddresses}},
+        HardDrives,
+        @{Name='MemoryAssignedMB'; Expression={$_.MemoryAssigned / 1MB}},
+        Uptime, 
+        Status
+        Write-Host "----------------------"
+    }
 }
 
 allVmInfo
