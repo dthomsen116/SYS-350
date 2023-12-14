1552d905502faf9770a52a80d65c582655ef9de2
dthomsen116 <71229951+dthomsen116@users.noreply.github.com>
2023-12-14 17:13:11 -0500

    Update main.ps1

--- Milestone 13/main.ps1
+++ Milestone 13/main.ps1
@@ -1,17 +1,12 @@
 
 function Get-HyperVVMInfo {
     # Get all VMs on the local Hyper-V server
-    $vms = Get-VM
-
-    foreach ($vm in $vms) {
-        $vmName = $vm.Name
-        $vmIP = $vm | Get-VMNetworkAdapter | Where-Object {$_.IPAddresses -ne $null} | Select-Object -ExpandProperty IPAddresses
-        $vmHostname = $vm | Get-VMIntegrationService | Where-Object {$_.Name -eq "Guest Service Interface"} | Select-Object -ExpandProperty PrimaryHostFullyQualifiedDomainName
-
-        # Output the VM information
-        write-host "VM Name: $vmName"
-        write-host "VM IP: $vmIP"
-        write-host "VM Hostname: $vmHostname"
-        write-host ""   
-    }
+    Get-VM | Select-Object -Property Name, State, 
+    
+    @{Name='IPAddresses'; Expression={($_.NetworkAdapters).IPAddresses}},
+    HardDrives,
+    
+    @{Name='MemoryAssignedMB'; Expression={$_.MemoryAssigned / 1MB}},
+    Uptime, 
+    Status
 }
